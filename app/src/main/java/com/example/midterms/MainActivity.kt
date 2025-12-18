5b4ed61fecfcaf8341360feaa585565d62c880d1
Zion Siason <zionandrewsiason3@gmail.com>
2025-12-18 18:28:28 +0800

    Added validation on the registration form(Main Activity)

--- app/src/main/java/com/example/midterms/MainActivity.kt
+++ app/src/main/java/com/example/midterms/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.core.view.WindowInsetsCompat
 import android.view.View
 import android.widget.CheckBox
 import android.widget.Toast
+import android.util.Patterns
 
 
 class MainActivity : AppCompatActivity() {
@@ -44,20 +45,50 @@ class MainActivity : AppCompatActivity() {
             val confirmPassword = confirmPasswordEditText.text.toString()
             val termsChecked = termsCheckBox.isChecked
 
-            if (username.isEmpty()) {
-                Toast.makeText(this, "Username can't be empty", Toast.LENGTH_SHORT).show()
-            } else if (username.length < 3) {
-                Toast.makeText(this, "Username must be at least 3 characters", Toast.LENGTH_SHORT).show()
-            } else {
-                Toast.makeText(this, "Account created successfully", Toast.LENGTH_SHORT).show()
+            when{
+                username.isEmpty() -> {
+                    Toast.makeText(this, "Username can't be empty", Toast.LENGTH_SHORT).show()
+                }
+                username.length < 3 -> {
+                    Toast.makeText(this, "Username must be at least 3 characters", Toast.LENGTH_SHORT).show()
+                }
+                email.isEmpty() -> {
+                    Toast.makeText(this, "Email can't be empty", Toast.LENGTH_SHORT).show()
+                }
+                !Patterns.EMAIL_ADDRESS.matcher(email).matches() -> {
+                    Toast.makeText(this, "Use a valid email address", Toast.LENGTH_SHORT).show()
+                }
+                password.isEmpty() -> {
+                    Toast.makeText(this, "Password can't be empty", Toast.LENGTH_SHORT).show()
+                }
+                password.length < 3 -> {
+                    Toast.makeText(this, "Password must be at least 3 characters", Toast.LENGTH_SHORT).show()
+                }
+                confirmPassword.isEmpty() -> {
+                    Toast.makeText(this, "Password can't be empty", Toast.LENGTH_SHORT).show()
+                }
+                confirmPassword != password -> {
+                    Toast.makeText(this, "Password must match", Toast.LENGTH_SHORT).show()
+                }
+                !termsChecked -> {
+                    Toast.makeText(this, "You must check the terms and conditions", Toast.LENGTH_SHORT).show()
+                }
 
-                val intent = Intent(this, SecondActivity::class.java)
-                intent.putExtra("username", username)
-                startActivity(intent)
+                else -> {
+                    Toast.makeText(this, "Account created successfully", Toast.LENGTH_SHORT).show()
+
+                    val intent = Intent(this, SecondActivity::class.java)
+                    intent.putExtra("username", username)
+                    startActivity(intent)
+
+                    usernameEditText.text.clear()
+                    emailEditText.text.clear()
+                    passwordEditText.text.clear()
+                    confirmPasswordEditText.text.clear()
+                    termsCheckBox.isChecked = false
+                }
+
             }
-
-            usernameEditText.text.clear()
-            passwordEditText.text.clear()
 
         }
 
